6f16ffb09dad35ffd8a4b9658d95cc979c1d6f3f
MrRezoo <rezam578@gmail.com>
2021-05-26 16:35:35 +0430

    extend cart url to base urls.py

--- django_online_shop/urls.py
+++ django_online_shop/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('accounts/', include('accounts.urls', namespace='accounts')),
-                  path('', include('shop.urls', namespace='shop')),
                   path('cart/', include('cart.urls', namespace='cart')),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('', include('shop.urls', namespace='shop')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
